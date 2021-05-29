c5ceccdfe671e65440d7f34257e5b059ff4610b2
Shweta2105 <sghadi21@gmail.com>
2021-05-29 07:24:41 +0530

    Added reverse no. program

---
+++ src/logicalProgramming/pailndrome.java
@@ -0,0 +1,20 @@
+package logicalProgramming;
+import java.util.Scanner;
+
+public class pailndrome {
+	public static void main(String[] args) {
+		System.out.println("Enter the number:");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+		int rev = 0;
+		while(num != 0) {
+			int temp = num%10;
+			rev = rev*10 + temp;
+			num /=10;
+			
+		}
+	System.out.println("The number reversed is : "+rev);
+	}
+
+}
