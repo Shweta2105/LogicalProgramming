45616dd39d272bc3f69836a98b6486af7c6c31a8
Shweta2105 <sghadi21@gmail.com>
2021-06-01 07:49:17 +0530

    Added binary conversion program

---
+++ src/logicalProgramming/binary.java
@@ -0,0 +1,37 @@
+package logicalProgramming;
+import java.util.Scanner;
+
+public class binary {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the number : ");
+		
+		int num = scan.nextInt();
+		
+		int bin = toBinary(num);
+		
+		//System.out.println("\nbinary conversion : "+bin);
+		
+	}
+
+	private static int toBinary(int num) {
+		
+		if(num > 1) {
+			toBinary(num/2);
+			
+		}
+		
+		int number = num%2;
+		String bin = "";
+		bin = bin + number;
+		System.out.print(bin);
+		int b = Integer.parseInt(bin);
+		//System.out.print(b);
+		
+		// TODO Auto-generated method stub
+		return b;
+	}
+
+}
